c7904f868acedfdc9424f20a7487952bc08828ad
An <16853175+ducan-ne@users.noreply.github.com>
2024-08-07 00:46:04 +0700

    feat: Improve navigation menu styling and font
    The code changes in this commit update the navigation menu styling and font in the `Header.astro` component. The font has been changed to "JetBrains Mono" for a more modern and consistent look. The styling has been improved with better spacing and underlines for active links. This enhancement improves the overall visual appeal and user experience of the website.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 // @ts-ignore
 import theme from './internal/react-dev/tailwind.config.js'
 import generated from '@tailwindcss/typography'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -8,7 +9,14 @@ export default {
 		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
 		'./node_modules/react-dev/src/components/MDX/Sandpack/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'
 	],
-	theme: theme,
+	theme: {
+		...theme,
+		fontFamily: {
+			...theme.fontFamily,
+			sans: ['Font', ...defaultTheme.fontFamily.sans],
+			mono: ['JetBrains Mono', ...defaultTheme.fontFamily.mono],
+		},
+	},
 	plugins: [
 		generated,
 	],
