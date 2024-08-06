b8960b5181fc7fb5d10b9259fb81ea22a3c0c52d
An <16853175+ducan-ne@users.noreply.github.com>
2024-08-07 01:08:01 +0700

    feat: Update font and image services, improve date formatting, and add about page
    This commit updates the font and image services in the `astro.config.mjs` file, replacing the `passthroughImageService` with `sharpImageService` and `squooshImageService`. It also improves the date formatting in the `FormattedDate.astro`, `index.astro`, and `bookmarks/index.astro` components by using the `format` function from the `date-fns` library. Additionally, it adds a new file `about.astro` which contains the content for the about page, providing information about the software engineer, Duc An. These changes enhance the website by improving the visual appeal, user experience, and providing more information to visitors.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,83 +1,88 @@
-import {defineConfig} from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import deno from '@astrojs/deno';
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import slug from 'rehype-slug'
-import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import {
+	defineConfig,
+	passthroughImageService,
+	sharpImageService,
+	squooshImageService,
+} from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import deno from "@astrojs/deno"
+import tailwind from "@astrojs/tailwind"
+import react from "@astrojs/react"
+import slug from "rehype-slug"
+import rehypeAutolinkHeadings from "rehype-autolink-headings"
 
 export default defineConfig({
-  site: 'https://an.cyou',
-  // site: 'http://localhost:4321',
-  integrations: [
-    react(),
-    mdx({
-      rehypePlugins: [slug, [rehypeAutolinkHeadings, {behavior: 'wrap'}]],
-    }),
-    sitemap({
-      serialize(item) {
-        // TODO filter draft blog
-        if (/priv/.test(item.url)) {
-          return undefined;
-        }
-        return item;
-      },
-    }),
-    tailwind(),
-  ],
-  output: 'server',
-  adapter: deno(),
-  vite: {
-    optimizeDeps: {
-      entries: ['eslint/lib/linter/linter', 'react-dev'],
-      disabled: false,
-      esbuildOptions: {
-        loader: {
-          '.js': 'jsx',
-        },
-      },
-      include: [
-        // 'eslint/lib/linter',
-        'react-dev > eslint/lib/linter',
-        'react',
-        'react-dom',
-        'anser',
-        'lz-string',
-        'escape-carriage',
-        'classnames',
-      ],
-    },
-    resolve: {
-      preserveSymlinks: true,
-      alias: {
-        '../../../../tailwind.config': `${process.cwd()}/internal/react-dev/tailwind.config.js`
-      }
-    },
-    build: {
-      onLog(level, log, handler) {
-        if (log.cause) {
-          // biome-ignore lint/suspicious/noConsoleLog: <explanation>
-          console.log(log.cause)
-          return
-        }
-        handler(level, log)
-      }
-    }
-  },
-  image: {},
-  markdown: {
-    shikiConfig: {
-      theme: 'github-dark-dimmed',
-      langs: [],
-      wrap: true
-    },
-    rehypePlugins: [
-      slug,
-      rehypeAutolinkHeadings,
-    ],
-  },
-  experimental: {
-    contentCollectionCache: true
-  }
-});
+	site: "https://an.cyou",
+	// site: 'http://localhost:4321',
+	integrations: [
+		react(),
+		mdx({
+			rehypePlugins: [slug, [rehypeAutolinkHeadings, { behavior: "wrap" }]],
+		}),
+		sitemap({
+			serialize(item) {
+				// TODO filter draft blog
+				if (/priv/.test(item.url)) {
+					return undefined
+				}
+				return item
+			},
+		}),
+		tailwind(),
+	],
+	output: "server",
+	adapter: deno({
+		// imageService: "passthrough",
+	}),
+	vite: {
+		optimizeDeps: {
+			entries: ["eslint/lib/linter/linter", "react-dev"],
+			disabled: false,
+			esbuildOptions: {
+				loader: {
+					".js": "jsx",
+				},
+			},
+			include: [
+				// 'eslint/lib/linter',
+				"react-dev > eslint/lib/linter",
+				"react",
+				"react-dom",
+				"anser",
+				"lz-string",
+				"escape-carriage",
+				"classnames",
+			],
+		},
+		resolve: {
+			preserveSymlinks: true,
+			alias: {
+				"../../../../tailwind.config": `${process.cwd()}/internal/react-dev/tailwind.config.js`,
+			},
+		},
+		build: {
+			onLog(level, log, handler) {
+				if (log.cause) {
+					// biome-ignore lint/suspicious/noConsoleLog: <explanation>
+					console.log(log.cause)
+					return
+				}
+				handler(level, log)
+			},
+		},
+	},
+	image: {
+		// service: import.meta.env.PROD
+		// 	? sharpImageService()
+		// 	: passthroughImageService(),
+	},
+	markdown: {
+		shikiConfig: {
+			theme: "github-light",
+			langs: [],
+			wrap: true,
+		},
+		rehypePlugins: [slug, rehypeAutolinkHeadings],
+	},
+})
--- src/content/blog/Chrome extension development tips.md
+++ src/content/blog/Chrome extension development tips.md
@@ -1,8 +1,8 @@
 ---
-title: Lazy loading React Component though Websocket and More
+title: Lazy loading React Component through Websocket and More
 description: Lorem ipsum dolor sit amet
 created: 2024-04-29T01:11
-updated: 2024-05-01T20:31
+updated: 2024-08-07T00:53
 draft: true
 ---
 - content scripts
